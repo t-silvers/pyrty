ddaa788702d02f0cbaea83415b8eb775c11fd8ce
t-silvers <silvers@molgen.mpg.de>
2023-07-12 20:09:28 +0200

    squash me

--- src/pyrty/pyr_func.py
+++ src/pyrty/pyr_func.py
@@ -3,6 +3,7 @@ import logging
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from typing import Dict, List, Union
+from enum import Enum
 
 import pandas as pd
 
@@ -14,35 +15,12 @@ from pyrty.utils import run_rscript
 _logger = logging.getLogger(__name__)
 _reg_manager = RegistryManager()
 
-
-def _default_env_name(s) -> str:
-    return f'{s}-env'
-
-def _default_env_prefix(s) -> Path:
-    return _reg_manager.envs / _default_env_name(s)
-
-def _default_rscript_path(s) -> Path:
-    return _reg_manager.scripts / f'{s}.R'
-
-def _check_env(env: Union[PyREnv, str, Path]):
-    if isinstance(env, (str, Path)):
-        raise NotImplementedError
-    elif isinstance(env, PyREnv):
-        return env
-    else:
-        raise NotImplementedError
-
-def _check_rscript(rscript: Union[PyRScript, Path, str]):
-    if isinstance(rscript, (str, Path)):
-        raise NotImplementedError
-    elif isinstance(rscript, PyREnv):
-        return rscript
-    else:
-        raise NotImplementedError
+class Cleanup(Enum):
+    ENV = 'env'
+    RSCRIPT = 'rscript'
 
 
 class PyRFunc:
-    
     _db_manager = DBManager()
     
     def __init__(self, alias, env, rscript):
@@ -55,6 +33,27 @@ class PyRFunc:
         # TODO: Allow for user-specified directory
         self.reg_manager = _reg_manager
     
+    @staticmethod
+    def _default_env_name(s) -> str:
+        return f'{s}-env'
+
+    @staticmethod
+    def _default_env_prefix(s) -> Path:
+        return _reg_manager.envs / PyRFunc._default_env_name(s)
+
+    @staticmethod
+    def _default_rscript_path(s) -> Path:
+        return _reg_manager.scripts / f'{s}.R'
+    
+    @staticmethod
+    def _check_obj(obj, cls_type):
+        if isinstance(obj, (str, Path)):
+            raise NotImplementedError
+        elif isinstance(obj, cls_type):
+            return obj
+        else:
+            raise NotImplementedError
+
     @property
     def args(self) -> List[str]:
         return self.rscript.rscript_manager.get_opts()
@@ -79,10 +78,7 @@ class PyRFunc:
         self._is_registered = True
 
     def run(self, cmd) -> Union[None, pd.DataFrame]:
-        _logger.info(
-            f'Running {self.alias}...'
-            f'\n\tCommand: {cmd}'
-        )
+        _logger.info(f'Running {self.alias}...\n\tCommand: {cmd}')
         return run_rscript(self.env.get_run_in_env_cmd(cmd), **self.run_kwargs)
 
     @atexit.register
@@ -94,10 +90,11 @@ class PyRFunc:
             - This is not called when the interpreter is killed by a signal.
             - This is not called when a thread exits.
         """
-        if len(self._delete_funcs) > 0:
+        if self._delete_funcs:
             print('Cleaning up...')
-            [f() for f in self._delete_funcs]
-            self._delete_funcs = set()
+            for func in self._delete_funcs:
+                func()
+            self._delete_funcs.clear()
 
     @classmethod
     def from_registry(cls, alias: str, alias_new: str = None):
@@ -108,11 +105,9 @@ class PyRFunc:
             but with different python class attributes, etc.
         """
         func = cls._db_manager.from_registry(alias)
-        if alias_new is None:
-            alias_new = alias
-        setattr(func, 'alias', alias_new)
+        setattr(func, 'alias', alias_new or alias)
         return func
-
+    
     def unregister(self):
         self._db_manager.unregister(self.alias)
         self._is_registered = False
@@ -123,52 +118,82 @@ class PyRFunc:
         if register:
             func.register(overwrite)
         else:
-            if 'env' in cleanup:
-                func._delete_funcs.add(func.env.remove_env())
-            if 'rscript' in cleanup:
-                func._delete_funcs.add(func.rscript.delete_file()) # remove R script on exit
+            for clean in cleanup:
+                if clean == Cleanup.ENV:
+                    func._delete_funcs.add(func.env.remove_env()) # remove env on exit
+                elif clean == Cleanup.RSCRIPT:
+                    func._delete_funcs.add(func.rscript.delete_file()) # remove R script on exit
 
         return func
 
     @classmethod
-    def from_rscript(cls, alias, rscript: Union[PyRScript, Path, str], manager: str = 'mamba',
-                     env_kwargs: Union[Dict, None] = None, register: bool = True, overwrite: bool = False) -> 'PyRFunc':
-        cls_rscript = _check_rscript(rscript)
-        cls_env = PyREnv(_default_env_name(alias), _default_env_prefix(alias), manager, env_kwargs=env_kwargs)
-        return cls._initialize(alias, cls_env, cls_rscript, register, overwrite=overwrite, cleanup=['env'])
+    def _create_rscript(cls, rscript_or_code, opts=None, libs=None, capture_output=False, capture_obj_name=None, 
+                        capture_type='df', skip_out_lines=0):
+        if isinstance(rscript_or_code, PyRScript):
+            return rscript_or_code
+        else:
+            code = rscript_or_code
+            return PyRScript(cls._default_rscript_path(alias), code, libs=libs, opts=opts,
+                             capture_output=capture_output, capture_obj_name=capture_obj_name,
+                             capture_type=capture_type, skip_out_lines=skip_out_lines)
+    
+    @classmethod
+    def _create_env(cls, env_or_manager, env_kwargs=None):
+        if isinstance(env_or_manager, PyREnv):
+            return env_or_manager
+        else:
+            manager = env_or_manager
+            return PyREnv(cls._default_env_name(alias), cls._default_env_prefix(alias), manager, env_kwargs=env_kwargs)
 
     @classmethod
-    def from_env(cls, alias, env: Union[PyREnv, str, Path], code: str, opts: Union[List, None] = None,
-                 libs: Union[List, None] = None, capture_output: bool = False, capture_obj_name : str = None,
-                 capture_type: str = 'df', skip_out_lines: int = 0, register: bool = True, overwrite: bool = False) -> 'PyRFunc':
-        cls_env = _check_env(env)
-        cls_rscript = PyRScript(_default_rscript_path(alias), code, libs=libs, opts=opts,
-                                capture_output=capture_output, capture_obj_name=capture_obj_name,
-                                capture_type=capture_type, skip_out_lines=skip_out_lines)
-        return cls._initialize(alias, cls_env, cls_rscript, register, overwrite=overwrite, cleanup=['rscript'])
+    def from_data(cls, alias, rscript_or_code, env_or_manager='mamba', opts=None, libs=None, capture_output=False, 
+                  capture_obj_name=None, capture_type='df', skip_out_lines=0, env_kwargs=None, register=True, 
+                  overwrite=False, cleanup=None) -> 'PyRFunc':
+        cls_rscript = cls._create_rscript(rscript_or_code, opts=opts, libs=libs, capture_output=capture_output,
+                                          capture_obj_name=capture_obj_name, capture_type=capture_type, 
+                                          skip_out_lines=skip_out_lines)
+        cls_env = cls._create_env(env_or_manager, env_kwargs=env_kwargs)
+        return cls._initialize(alias, cls_env, cls_rscript, register, overwrite=overwrite, cleanup=cleanup)
+
+    @classmethod
+    def from_rscript(cls, alias, rscript, manager: str = 'mamba', env_kwargs: Union[Dict, None] = None,
+                     register: bool = True, overwrite: bool = False) -> 'PyRFunc':
+        return cls.from_data(alias, rscript, manager, env_kwargs=env_kwargs, register=register, 
+                             overwrite=overwrite, cleanup=['env'])
+
+    @classmethod
+    def from_env(cls, alias, env, code: str, opts: Union[List, None] = None, libs: Union[List, None] = None,
+                 capture_output: bool = False, capture_obj_name : str = None,
+                 capture_type: str = 'df', skip_out_lines: int = 0, register: bool = True, 
+                 overwrite: bool = False) -> 'PyRFunc':
+        return cls.from_data(alias, code, env, opts=opts, libs=libs, capture_output=capture_output,
+                             capture_obj_name=capture_obj_name, capture_type=capture_type, 
+                             skip_out_lines=skip_out_lines, register=register, overwrite=overwrite, 
+                             cleanup=['rscript'])
 
     @classmethod
     def from_scratch(cls, alias, code, opts: Union[List, None] = None, libs: Union[List, None] = None,
                      capture_output: bool = False, capture_obj_name : str = None, capture_type: str = 'df',
                      skip_out_lines: int = 0, manager: str = 'mamba', env_kwargs: Union[Dict, None] = None,
                      register: bool = True, overwrite: bool = False) -> 'PyRFunc':
-        cls_env = PyREnv(_default_env_name(alias), _default_env_prefix(alias), manager, env_kwargs=env_kwargs)        
-        cls_rscript = PyRScript(_default_rscript_path(alias), code, libs=libs, opts=opts,
-                                capture_output=capture_output, capture_obj_name=capture_obj_name,
-                                capture_type=capture_type, skip_out_lines=skip_out_lines)
-        return cls._initialize(alias, cls_env, cls_rscript, register, overwrite=overwrite, cleanup=['rscript', 'env'])
+        return cls.from_data(alias, code, manager, opts=opts, libs=libs, capture_output=capture_output,
+                             capture_obj_name=capture_obj_name, capture_type=capture_type, 
+                             skip_out_lines=skip_out_lines, env_kwargs=env_kwargs, register=register, 
+                             overwrite=overwrite, cleanup=['rscript', 'env'])
 
     def __call__(self, finput=None) -> Union[pd.DataFrame, None]:
         with TemporaryDirectory() as tmpdirname:
-            run_cmd = f'Rscript {self.rscript_path} '
+            opts = []
             if finput:
                 for opt_name, opt_val in finput.items():
                     if isinstance(opt_val, pd.DataFrame):
                         tmpfile = Path(tmpdirname) / f'{opt_name}.csv'
                         opt_val.to_csv(tmpfile, index=False)
-                        run_cmd += f"--{opt_name} {tmpfile} "
+                        opts.append(f"--{opt_name} {tmpfile}")
                     else:
-                        run_cmd += f"--{opt_name} {opt_val} "
+                        opts.append(f"--{opt_name} {opt_val}")
+
+            run_cmd = f'Rscript {self.rscript_path} {" ".join(opts)}'
             foutput = self.run(run_cmd)
         return foutput
 
